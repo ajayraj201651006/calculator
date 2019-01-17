6c2688a630a4e2e5fc70c224560bb7da0d48a1ea
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-17 14:54:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
+import Result from './components/Result/Result';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.App}>
         <Layout>
-           <h1>Hello</h1>
+          <h1>Hello</h1>
         </Layout>
       </div>
     );
---
+++ src/components/Result/Result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Result.css';
+
+const result = () => (
+        <div className={classes.Result}>
+            <h1>Hello</h1>
+        </div>
+	);
+
+export default result
